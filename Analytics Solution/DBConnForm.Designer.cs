57593964995a58f5cd40972239e0755f4ec7ded9
Daniel Moore <daniel.moore@panderasystems.com>
2014-07-31 11:57:56 -0400

    adds db connection form

--- Analytics Solution/DBConnForm.Designer.cs
+++ Analytics Solution/DBConnForm.Designer.cs
@@ -28,11 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbxConnections = new System.Windows.Forms.ComboBox();
+            this.lblConnections = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Connection";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cmbxConnections
+            // 
+            this.cmbxConnections.FormattingEnabled = true;
+            this.cmbxConnections.Location = new System.Drawing.Point(12, 39);
+            this.cmbxConnections.Name = "cmbxConnections";
+            this.cmbxConnections.Size = new System.Drawing.Size(606, 24);
+            this.cmbxConnections.TabIndex = 1;
+            // 
+            // lblConnections
+            // 
+            this.lblConnections.AutoSize = true;
+            this.lblConnections.Location = new System.Drawing.Point(13, 13);
+            this.lblConnections.Name = "lblConnections";
+            this.lblConnections.Size = new System.Drawing.Size(142, 16);
+            this.lblConnections.TabIndex = 2;
+            this.lblConnections.Text = "Available Connections";
+            this.lblConnections.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // DBConnForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 120);
+            this.Controls.Add(this.lblConnections);
+            this.Controls.Add(this.cmbxConnections);
+            this.Controls.Add(this.button1);
+            this.Name = "DBConnForm";
             this.Text = "DBConnForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbxConnections;
+        private System.Windows.Forms.Label lblConnections;
     }
 }
--- Analytics Solution/DBConnForm.cs
+++ Analytics Solution/DBConnForm.cs
@@ -16,5 +16,10 @@ namespace Analytics_Solution
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
