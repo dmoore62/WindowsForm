2fbf5abc72e5388e660c183f73024d7522aa1f93
Alex Gordon <alex.c.gordon@gmail.com>
2014-07-31 17:04:20 -0400

    new send of sql

--- Analytics Solution/Properties/Resources.Designer.cs
+++ Analytics Solution/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Analytics_Solution.Properties
-{
-
-
+namespace Analytics_Solution.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,72 @@ namespace Analytics_Solution.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Analytics_Solution.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to IF EXISTS(select * from sys.databases where name = &apos;pandera_metadata&apos;)
+        ///DROP DATABASE pandera_metadata
+        ///
+        ///CREATE DATABASE pandera_metadata
+        ///
+        ///USE pandera_metadata
+        ///
+        ///CREATE TABLE project(
+        ///	
+        ///	id INT IDENTITY not null,
+        ///	name VARCHAR(50) not null,
+        ///	created DATETIME not null,
+        ///	DB_File TEXT not null,
+        ///	CONSTRAINT project_pk_id PRIMARY KEY (id),
+        ///	CONSTRAINT project_unique_name UNIQUE (name)
+        ///);
+        ///
+        ///CREATE TABLE attribute(
+        ///	
+        ///	id INT IDENTITY not null,
+        ///	name VARCHAR(50) not null,
+        ///	descr TEXT not null,
+        ///	pi [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string master_script {
+            get {
+                return ResourceManager.GetString("master_script", resourceCulture);
             }
         }
     }
