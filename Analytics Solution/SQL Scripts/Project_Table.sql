2cc7d2ae66d949b5092b6ae3d7c3648c2dff6269
Alex Gordon <alex.c.gordon@gmail.com>
2014-07-31 16:49:47 -0400

    Master_Script

--- Analytics Solution/SQL Scripts/Project_Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE project(
-	
-	id INT IDENTITY not null,
-	name VARCHAR(50) not null,
-	created DATETIME not null,
-	DB_File TEXT not null,
-	CONSTRAINT project_pk_id PRIMARY KEY (id),
-	CONSTRAINT project_unique_name UNIQUE (name)
-);
