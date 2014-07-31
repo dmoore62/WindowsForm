ccc0009d1a34abd930e9f4b1908abaea1264fbbd
Daniel Moore <daniel.moore@panderasystems.com>
2014-07-31 13:30:11 -0400

    adds db connection screen

--- Analytics Solution/DBConnForm.Designer.cs
+++ Analytics Solution/DBConnForm.Designer.cs
@@ -29,44 +29,65 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.cmbxConnections = new System.Windows.Forms.ComboBox();
             this.lblConnections = new System.Windows.Forms.Label();
+            this.tbxConnInput = new System.Windows.Forms.TextBox();
+            this.btnConnTest = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(461, 82);
+            this.button1.Location = new System.Drawing.Point(461, 61);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.Size = new System.Drawing.Size(157, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "Open Connection";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // cmbxConnections
-            // 
-            this.cmbxConnections.FormattingEnabled = true;
-            this.cmbxConnections.Location = new System.Drawing.Point(12, 39);
-            this.cmbxConnections.Name = "cmbxConnections";
-            this.cmbxConnections.Size = new System.Drawing.Size(606, 24);
-            this.cmbxConnections.TabIndex = 1;
             // 
             // lblConnections
             // 
             this.lblConnections.AutoSize = true;
             this.lblConnections.Location = new System.Drawing.Point(13, 13);
             this.lblConnections.Name = "lblConnections";
-            this.lblConnections.Size = new System.Drawing.Size(142, 16);
+            this.lblConnections.Size = new System.Drawing.Size(115, 16);
             this.lblConnections.TabIndex = 2;
-            this.lblConnections.Text = "Available Connections";
+            this.lblConnections.Text = "Connection Name";
             this.lblConnections.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbxConnInput
+            // 
+            this.tbxConnInput.Location = new System.Drawing.Point(16, 33);
+            this.tbxConnInput.Name = "tbxConnInput";
+            this.tbxConnInput.Size = new System.Drawing.Size(602, 22);
+            this.tbxConnInput.TabIndex = 3;
+            // 
+            // btnConnTest
+            // 
+            this.btnConnTest.Location = new System.Drawing.Point(320, 61);
+            this.btnConnTest.Name = "btnConnTest";
+            this.btnConnTest.Size = new System.Drawing.Size(135, 34);
+            this.btnConnTest.TabIndex = 4;
+            this.btnConnTest.Text = "Test Connection";
+            this.btnConnTest.UseVisualStyleBackColor = true;
+            this.btnConnTest.Click += new System.EventHandler(this.btnConnTest_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(16, 78);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 16);
+            this.lblStatus.TabIndex = 5;
             // 
             // DBConnForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(630, 120);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnConnTest);
+            this.Controls.Add(this.tbxConnInput);
             this.Controls.Add(this.lblConnections);
-            this.Controls.Add(this.cmbxConnections);
             this.Controls.Add(this.button1);
             this.Name = "DBConnForm";
             this.Text = "DBConnForm";
@@ -78,7 +99,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox cmbxConnections;
         private System.Windows.Forms.Label lblConnections;
+        private System.Windows.Forms.TextBox tbxConnInput;
+        private System.Windows.Forms.Button btnConnTest;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
