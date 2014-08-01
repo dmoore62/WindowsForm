072d60867d869f2b9a07bc5f8bb03bfeeca3757b
Alex Gordon <alex.c.gordon@gmail.com>
2014-08-01 14:53:56 -0400

    trying this

---
+++ Analytics Solution/Attribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Analytics_Solution
+{
+    class Attribute
+    {
+
+        
+        private List<int> attr_id;
+        private List<string> attr_name;
+        private List<string> attr_descr;
+        private List<int> attr_pid;
+        private List<DateTime> attr_time;
+         
+
+
+        public Attribute(int attr_id, string attr_name, string attr_descr, int attr_pid)
+        {
+            DateTime time = DateTime.Now;
+
+
+            this.attr_id.Add(attr_id);
+            this.attr_name.Add(attr_name);
+            this.attr_descr.Add(attr_descr);
+            this.attr_pid.Add(attr_pid);
+            attr_time.Add(time);
+
+        }
+
+
+
+    }
+}
---
+++ Analytics Solution/local_db.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analytics_Solution
+{
+    class Local_DB
+    {
+
+        private List<Attribute> attr;
+         
+
+
+        public Local_DB(Attribute attr)
+        {
+            this.attr.Add(attr);
+
+        }
+
+
+
+
+
+    }
+}
