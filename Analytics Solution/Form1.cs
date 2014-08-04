2c5e1fea317805df076f78ee3a7af635e8245610
Daniel Moore <daniel.moore@panderasystems.com>
2014-08-04 14:55:55 -0400

    writes reference schema to local data file

--- Analytics Solution/Form1.cs
+++ Analytics Solution/Form1.cs
@@ -220,6 +220,12 @@ namespace Analytics_Solution
         }
 
         public void createNewDB(String fileName) {
+            String fn = fileName;
+            createSchema(fn);
+            populateSchemaReference();
+        }
+
+        public void createSchema(String fileName) {
             String conStr = "data source=" + fileName;
             Debug.WriteLine(fileName);
             SQLiteConnection.CreateFile(fileName);
@@ -239,6 +245,32 @@ namespace Analytics_Solution
                     finally {
                         con.Close();
                     }                
+                }
+            }
+        }
+
+        public void populateSchemaReference(){
+            String conStr = this.WriteConStr;
+            ProjectDB db = this.projectDb;
+            var simpleList = db.getSimpleList();
+            using (SQLiteConnection con = new SQLiteConnection(conStr)) {
+                using (SQLiteCommand cmd = new SQLiteCommand(con)) {
+                    try {
+                        con.Open();
+                        String sql = "INSERT INTO table_reference (description) VALUES (@d)";
+                        cmd.CommandText = sql;
+                        foreach (var element in simpleList) {
+                            cmd.Parameters.AddWithValue("@d", element);
+                            cmd.ExecuteNonQuery();
+                            Debug.WriteLine(element);
+                        }
+                    }
+                    catch (Exception ex) {
+                        throw new Exception("Error inserting reference: " + ex.Message);
+                    }
+                    finally {
+                        con.Close();
+                    }
                 }
             }
         }
