c5a7daac38f3e06b8b73b2fcc121ca19257d1d6c
Daniel Moore <daniel.moore@panderasystems.com>
2014-08-04 16:17:33 -0400

    creates DB object from import

---
+++ Analytics Solution/ErrorForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Analytics_Solution
+{
+    partial class ErrorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDismiss = new System.Windows.Forms.Button();
+            this.lblErrorText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDismiss
+            // 
+            this.btnDismiss.Location = new System.Drawing.Point(152, 127);
+            this.btnDismiss.Name = "btnDismiss";
+            this.btnDismiss.Size = new System.Drawing.Size(149, 46);
+            this.btnDismiss.TabIndex = 0;
+            this.btnDismiss.Text = "Close";
+            this.btnDismiss.UseVisualStyleBackColor = true;
+            this.btnDismiss.Click += new System.EventHandler(this.btnDismiss_Click);
+            // 
+            // lblErrorText
+            // 
+            this.lblErrorText.AutoSize = true;
+            this.lblErrorText.Location = new System.Drawing.Point(79, 58);
+            this.lblErrorText.Name = "lblErrorText";
+            this.lblErrorText.Size = new System.Drawing.Size(98, 17);
+            this.lblErrorText.TabIndex = 1;
+            this.lblErrorText.Text = "Error Occured";
+            // 
+            // ErrorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 185);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblErrorText);
+            this.Controls.Add(this.btnDismiss);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "ErrorForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDismiss;
+        private System.Windows.Forms.Label lblErrorText;
+    }
+}
--- Analytics Solution/Form1.cs
+++ Analytics Solution/Form1.cs
@@ -283,8 +283,64 @@ namespace Analytics_Solution
         private void BtnCreateDataBase_Click(object sender, EventArgs e)
         {
             Debug.WriteLine("Clicked");
-            DBConnForm inputform = new DBConnForm(this);
-            inputform.Show();
+            //DBConnForm inputform = new DBConnForm(this);
+            //inputform.Show();
+
+            //lets try this with a dialog box instead
+            dlgDatabaseFile.Multiselect = false;
+            if (dlgDatabaseFile.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                Debug.WriteLine(dlgDatabaseFile.FileName);
+                //set project connections
+                if (dlgDatabaseFile.FileName.ToLower().EndsWith(".sqlite"))
+                {
+                    this.WriteConnection = dlgDatabaseFile.FileName;
+                    this.WriteConStr = "data source=" + dlgDatabaseFile.FileName;
+                    //reverse engineer projectDB object from datasource table
+                    populateProjectDB();
+                }
+                else {
+                    ErrorForm ef = new ErrorForm("Error on Import", "An invalid file type was selected");
+                    ef.Show();
+                }
+            }
+        }
+
+        private void populateProjectDB() { 
+            String conStr = this.WriteConStr;
+            String sql;
+            List<String> objVals = new List<String>();
+            //pull all records from sqlite file
+            using (SQLiteConnection con = new SQLiteConnection(conStr)) {
+                using (SQLiteCommand cmd = new SQLiteCommand(con)) {
+                    try {
+                        con.Open();
+                        sql = "SELECT * FROM table_reference";
+
+                        cmd.CommandText = sql;
+                        using (SQLiteDataReader reader = cmd.ExecuteReader()) {
+                            if (reader.HasRows) {
+                                while (reader.Read()) {
+                                    objVals.Add(reader["description"].ToString());
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex) {
+                        throw new Exception("Error cannot populate db object: " + ex.Message);
+                    }
+                    finally {
+                        con.Close();
+                    }
+                }
+            }
+
+            //create DB object
+            this.projectDb = new ProjectDB(objVals);
+
+            this.Text = this.Text + " - " + this.WriteConnection;
+            Label lblImport = (Label)this.lblImportStatus;
+            lblImport.ForeColor = Color.Green;
+            lblImport.Text = "Imported - " + this.WriteConnection;
         }
     }
 }
